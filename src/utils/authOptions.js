1ad0146771170e81ab30e730541a05b45faf855a
mdferdousalam <mdferdousalam1989@yahoo.com>
2025-01-13 15:49:31 +0600

    Add authentication and user management features with NextAuth integration

---
+++ src/utils/authOptions.js
@@ -0,0 +1,20 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
